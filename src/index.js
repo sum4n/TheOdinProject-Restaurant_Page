c54245bed205e30985ab1ae856a4daa2d6fc19e9
Suman Das <sum4ndas@gmail.com>
2022-06-30 09:40:45 +0530

    Create bare-bones homepage using JavaScript only

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("I'm running!");
+import firstPage from './firstPage';
+
+const content = document.getElementById("content");
+
+content.appendChild(firstPage().h1);
+content.appendChild(firstPage().moto);
+content.appendChild(firstPage().img);
+content.appendChild(firstPage().info);
