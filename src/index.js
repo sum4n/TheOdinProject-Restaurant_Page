3f62fb01e79c82c62be6a102e08a8430b2e3d712
Suman Das <sum4ndas@gmail.com>
2022-07-14 20:05:49 +0530

    Add background image to body to keep background on page scrolldown

--- src/index.js
+++ src/index.js
@@ -9,17 +9,18 @@ import contactPage from "./contactPage";
 
 const content = document.getElementById("content");
 
+const bodyDiv = document.querySelector('body');
+const bImg = document.createElement('img');
+bImg.classList.add('background');
+bImg.src = gsInn;
+bodyDiv.appendChild(bImg);
+
 content.appendChild(tab());
 // content.appendChild(homePage());
 content.appendChild(menuPage());
 // set homeTab marker on page load as it loads first automatically
 document.getElementById('homeTab').classList.add('underline');
 
-const bodyDiv = document.querySelector('body');
-const bImg = document.createElement('img');
-bImg.classList.add('background');
-bImg.src = gsInn;
-bodyDiv.appendChild(bImg);
 
 function removeDiv() {
     const lastChild = content.lastElementChild;
